516a2ebb4be9955fd420af59ecb1c19d2aedf3c4
TSimpson <tomsimpson11@hotmail.co.uk>
2020-02-27 16:32:28 +0100

    Initial Commit

---
+++ spracheappTests/spracheappTests.swift
@@ -0,0 +1,34 @@
+//
+//  spracheappTests.swift
+//  spracheappTests
+//
+//  Created by Thomas Simpson on 27.02.20.
+//  Copyright © 2020 Thomas Simpson. All rights reserved.
+//
+
+import XCTest
+@testable import spracheapp
+
+class spracheappTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
